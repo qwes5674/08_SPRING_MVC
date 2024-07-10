d98154daedd9df7b40d071949c5fdb223a169bf6
qwes5674 <qwes5674@gmail.com>
2024-07-10 14:29:37 +0900

    interceptor

--- chap05-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/InterceptorController.java
+++ chap05-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/InterceptorController.java
@@ -1,2 +1,21 @@
-package com.ohgiraffers.interceptor;public class Interceptor {
+package com.ohgiraffers.interceptor;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/*")
+public class InterceptorController {
+
+    @GetMapping("stopwatch")
+    public String handlerMethod() throws InterruptedException {
+        System.out.println("핸들러 메소드 호출함...");
+
+        // 1초동안 기달렸다가 다음 코드 실행
+        Thread.sleep(1000);
+
+        return "result";
+    }
 }
